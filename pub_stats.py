06d25964bc541c8789a286789cd50b72e499941c
adamnip <75849133+adamnip@users.noreply.github.com>
2020-12-18 12:48:40 +0000

    Add files via upload

--- pub_stats.py
+++ pub_stats.py
@@ -3,8 +3,7 @@ import json
 import paho.mqtt.publish as publish
 import argparse
 
-'''mqtt details'''
-hostname = ""
+hostname = "mqtt.home.com"
 auth={'username': "", 'password': ""}
 
 float_keys_list = ["UPS_ALARMDEL", "UPS_BATTV",	"UPS_BCHARGE", "UPS_CUMONBATT", "UPS_HITRANS", "UPS_LINEV", "UPS_LOADPCT", "UPS_LOTRANS", "UPS_MAXTIME", "UPS_MBATTCHG", "UPS_MINTIMEL", "UPS_NOMBATTV", "UPS_NOMINV", "UPS_NOMPOWER", "UPS_TIMELEFT", "UPS_TONBATT",]
@@ -34,7 +33,11 @@ def autopub():
             message = int(float(value))
         else:
             message = (dict[i])
-        publish.single (topic, message, hostname=hostname, keepalive=5,retain=True)
+        '''Is username empty'''
+        if auth['username'] != "":
+            publish.single (topic, message, hostname=hostname, auth = auth, keepalive=5,retain=True)
+        else:
+            publish.single (topic, message, hostname=hostname, keepalive=5,retain=True)
 
 if __name__ == '__main__':
     data = get_stats()
